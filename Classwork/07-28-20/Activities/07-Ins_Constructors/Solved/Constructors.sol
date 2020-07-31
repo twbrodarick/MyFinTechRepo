5d709815c5cb1a6c4146e20e7c219aff23e6dec4
twbrodarick <tbrodarick@gmail.com>
2020-07-30 21:31:15 -0500

    July 30 classwork

---
+++ Classwork/07-28-20/Activities/07-Ins_Constructors/Solved/Constructors.sol
@@ -0,0 +1,53 @@
+pragma solidity ^0.5.11;
+
+contract JointSavings {
+  address payable account_one;
+  address payable account_two;
+
+  address public last_to_withdraw;
+  uint public last_withdraw_block;
+  uint public last_withdraw_amount;
+
+  address public last_to_deposit;
+  uint public last_deposit_block;
+  uint public last_deposit_amount;
+
+  uint unlock_time;
+
+  constructor(address payable _one, address payable _two) {
+    account_one = _one;
+    account_two = _two;
+  }
+
+ 
+
+  function withdraw(uint amount) public {
+    require(unlock_time >=block.timestamp, "Account is locked!");
+    require(msg.sender == account_one || msg.sender == account_two, "You don't own this account!");
+
+    if (last_to_withdraw != msg.sender) {
+      last_to_withdraw = msg.sender;
+    }
+
+    last_withdraw_block = block.number;
+    last_withdraw_amount = amount;
+
+    if (amount > address(this).balance / 4) {
+      unlock_time = block.timestamp + 24 hours;
+    }
+
+    msg.sender.transfer(amount);
+  }
+
+  function deposit() public payable {
+
+    if (last_to_deposit != msg.sender) {
+      last_to_deposit = msg.sender;
+    }
+
+    last_deposit_block = block.number;
+    last_deposit_amount = msg.value;
+  }
+
+  function() external payable {}
+}
